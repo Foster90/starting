40dbb1bb0718a98713455e2a57cc192a006bfded
Foster90 <matthew.foster90@hotmail.com>
2020-11-03 19:04:47 +0000

    Add new CampsController update Autofac settings

--- TheCodeCamp/Controllers/CampsController.cs
+++ TheCodeCamp/Controllers/CampsController.cs
@@ -1,18 +1,66 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
+//using System.Web.Mvc;
+using TheCodeCamp.Data;
+using TheCodeCamp.Data.Migrations;
 
 namespace TheCodeCamp.Controllers
 {
+
+    [RoutePrefix("api/camps")]
     public class CampsController : ApiController
     {
-        // GET: Camps
-        public object Get()
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+
+
+        public CampsController(ICampRepository repository, IMapper mapper)
         {
-           return new { Name = "Shawn", Occupation = "Teacher"};
+            _repository = repository;
+            _mapper = mapper;
         }
+
+        [Route()]
+        public async Task<IHttpActionResult> Get(bool includeTalks = false)
+        {
+            try
+            {
+                var result = await _repository.GetAllCampsAsync(includeTalks);
+
+                var mappedResult = _mapper.Map<IEnumerable<CampModel>>(result);
+
+                return Ok(mappedResult);
+            }
+
+            catch(Exception ex)            
+            {
+                //TODO Add Logging
+                return InternalServerError(ex);
+            }
+        }
+
+        [Route("{moniker}")]
+        public async Task<IHttpActionResult> Get(string moniker)
+        {
+            try 
+            {
+                var result = await _repository.GetCampAsync(moniker);
+                if (result == null) return NotFound();
+
+                return Ok(_mapper.Map<CampModel>(result));
+            }
+
+            catch (Exception ex)
+            { 
+                return InternalServerError(ex); 
+            }
+
+        }
+
     }
 }
